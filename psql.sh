378d587550550cf4701b3d760616abe92611e867
R Dama <damarak630@gmail.com>
2017-06-12 11:50:50 -0700

    Create psql.sh

--- psql.sh
+++ psql.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
-tar -zxvf $1/postgresql-*.tar.gz --strip-components 1 -C /opt/psql;
-cd /opt/psql
-./configure
-gmake
-gmake install
-useradd postgres
-mkdir /opt/psql/data
-chown postgres /opt/psql/data
-su - postgres
-/opt/psql/bin/initdb -D /opt/psql/data
-/opt/psql/bin/postmaster -i -D /opt/psql/data >logfile 2>&1 &
+#tar -zxvf $1/postgresql-*.tar.gz --strip-components 1 -C /opt/psql;
+mkdir -p /opt/pgsql/data
+tar -zxvf ./../Packages/postgresql-*.tar.gz --strip-components 1 -C /opt/pgsql;
+cd /opt/pgsql
+#./configure
+#make
+#make install
+#useradd postgres||true
+set -xe
+id -u postgres &>/dev/null || useradd postgres
+chown -R postgres /opt/pgsql/data
+su - postgres <<-'EOF'
+/opt/pgsql/bin/initdb -D /opt/pgsql/data
+/opt/pgsql/bin/postmaster -i -D /opt/pgsql/data >logfile 2>&1 &
+/opt/pgsql/bin/createdb test
+EOF
