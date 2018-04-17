bb9e4db16ff6c84b4e6a775acc531dc102d4ad59
R Dama <damarak630@gmail.com>
2018-04-17 15:27:27 -0700

    Update Array-Commandinput.sh

--- Array-Commandinput.sh
+++ Array-Commandinput.sh
@@ -7,14 +7,18 @@ pw="Passw0rd"
 
 
 get_installedversions(){
-array=($(net rpc service list -I $host -U $domain/$user%$pw | grep -i SQL))
+array=( $(net rpc service list -I $host -U $domain/$user%$pw | grep -i SQL) )
 len=${#array[*]}
 echo "The array has length $len members. They are:"
 
 i=0
 while [ $i -lt $len ]; do
     echo "$i: ${array[$i]}"
+    declare -a "var$i=${array[$i]}"
     let i++
 done
+echo -n "Enter the service to be activated: "
+    read version
 }
 get_installedversions
+echo selected service is ${array[$version]}
