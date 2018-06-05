2ab032d0ebc958809fe71773f0e60a71de9a4807
R Dama <damarak630@gmail.com>
2018-06-05 14:43:16 -0700

    Update Install-Python.sh

--- Install-Python.sh
+++ Install-Python.sh
@@ -1,1 +1,35 @@
+#!/bin/bash
 
+Pkgname="";
+Install_Dir="/opt/smarts"
+
+check_version() {
+echo "checking if version already exists"
+
+if command -v python3 &>/dev/null; then
+echo python 3 is installed
+else
+echo python3 is not installed
+fi
+
+}
+
+install() {
+cd 
+tar -zxvf Python-3.5.2.tar.gz
+cd Python-3.5.2
+./configure --prefix=$Install_Dir
+make && make altinstall
+}
+
+post-install() {
+echo adding it to path, if not already there
+[[ ":$PATH:" !=  *":${Install_Dir}/:"* && PATH="${Install_Dir}:${PATH}" ]]
+}
+
+setuptools() {
+cd 
+tar -zxvf setuptools-1.4.2.tar.gz
+cd setuptools-1.4.2
+python3 setup.py install
+} 
